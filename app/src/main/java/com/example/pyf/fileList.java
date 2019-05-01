4622c12110054ee93c7c63d25260c5e1e91d29f1
ScottHK <scotthkinsey@gmail.com>
2019-05-01 06:20:25 +0100

    Implemented delete feature to file list

--- app/src/main/java/com/example/pyf/fileList.java
+++ app/src/main/java/com/example/pyf/fileList.java
@@ -6,25 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.File;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class fileList extends AppCompatActivity {
-    TextView transaction;
+    boolean fileDelete = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_list);
-        ListView li = findViewById(R.id.lv_Files);
+        final ImageButton delete = findViewById(R.id.ib_delete);
+        final ListView li = findViewById(R.id.lv_Files);
         String[] array = fileList();
         ArrayList<String> lst = new ArrayList<>(Arrays.asList(array));
         final ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
@@ -36,11 +34,46 @@ public class fileList extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String selected = (String) parent.getItemAtPosition(position);
 
-                Intent openFile = new Intent(getApplicationContext(), BudgetHome.class);
-                openFile.putExtra("fileName", selected);
-                Toast toast = Toast.makeText(getApplicationContext(), selected, Toast.LENGTH_SHORT);
-                toast.show();
-                startActivity(openFile);
+                if (fileDelete) {
+
+                    File file = new File(getFilesDir() + selected);
+                    deleteFile(selected);
+
+                    if (!file.exists()) {
+                        adapter.remove(adapter.getItem(position));
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "File deleted successfully", Toast.LENGTH_SHORT);
+                        toast.show();
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "Failed to delete the file", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                } else {
+                    Intent openFile = new Intent(getApplicationContext(), BudgetHome.class);
+                    openFile.putExtra("fileName", selected);
+                    Toast toast = Toast.makeText(getApplicationContext(), selected, Toast.LENGTH_SHORT);
+                    toast.show();
+                    startActivity(openFile);
+                }
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(fileDelete) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Delete Mode: Off", Toast.LENGTH_SHORT);
+                    toast.show();
+                    fileDelete = false;
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Delete Mode: On", Toast.LENGTH_SHORT);
+                    toast.show();
+                    fileDelete = true;
+                }
             }
         });
     }
